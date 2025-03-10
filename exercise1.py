a1c62082e6be1cc9b5ba06f084f310d001dd7d2a
Arc_U <tammy.hills@geoexcenter.org>
2025-03-09 18:37:36 -0600

    Initial Input

---
+++ exercise1.py
@@ -0,0 +1,28 @@
+import arcpy
+arcpy.env.workspace = r"C:\Users\Arc_U\Desktop\GIS3005\Assignment1\Admin\AdminData.gdb"
+arcpy.env.overwriteOutput = True
+arcpy.SelectLayerByAttribute_management("cities", "CLEAR_SELECTION")
+
+flayer = arcpy.MakeFeatureLayer_management("cities", "Cities_Layer")
+
+qry = "POP1990 > 20000"
+arcpy.management.SelectLayerByAttribute(flayer, "NEW_SELECTION", qry)
+
+my_cnt = arcpy.management.GetCount(flayer)
+print(f"Selected cities is: {my_cnt}")
+
+arcpy.management.SelectLayerByLocation(flayer, "WITHIN_A_DISTANCE", "us_rivers", "10 miles", "SUBSET_SELECTION")
+
+my_cnt = arcpy.management.GetCount(flayer)
+print(f"Selected cities is: {my_cnt}")
+
+field = 'POP1990'
+total = 0
+i = 1
+with arcpy.da.SearchCursor(flayer, field) as cursor:
+    for row in cursor:
+        print(i, str(row[0]))
+        total = total + row[0]
+        i = i + 1
+
+print(f"Total population is: {total:,}")
